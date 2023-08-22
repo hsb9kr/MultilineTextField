f530c4551f479617f793c718cac102535ce7b002
Red <hsb9kr@gmail.com>
2023-08-22 10:04:43 +0900

    Initial Commit

---
+++ MultilineTextField/MultilineTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultilineTextFieldApp.swift
+//  MultilineTextField
+//
+//  Created by Red on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MultilineTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
